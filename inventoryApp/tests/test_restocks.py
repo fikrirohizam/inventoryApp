a9a79a7a6c3776cb6839f4a70af5bd2699afdd05
fikrirohizam <fikri@infin8co.com>
2023-03-29 14:50:53 +0800

    add unit test for restock all

--- inventoryApp/tests/test_restocks.py
+++ inventoryApp/tests/test_restocks.py
@@ -62,6 +62,27 @@ class RestocksViewTestCase(APITestCase):
         stock2.refresh_from_db()
         self.assertEqual(stock2.current_capacity, 210)
 
+    def test_add_no_materials(self):
+        self.authenticate()
+        material1 = MaterialFactory.create(price=100.0)
+        stock1 = MaterialStockFactory(material=material1, store=self.store, current_capacity=50, max_capacity=100)
+        material2 = MaterialFactory.create(price=200.0)
+        stock2 = MaterialStockFactory(material=material2, store=self.store, current_capacity=30, max_capacity=50)
+
+        data = {}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['materials'][0]['total_price'], 5000.0)
+        self.assertEqual(response.data['materials'][1]['total_price'], 4000.0)
+        self.assertEqual(response.data['materials'][0]['quantity'], 50)
+        self.assertEqual(response.data['materials'][1]['quantity'], 20)
+
+        self.assertEqual(response.data['overall_price'], 9000.0)
+        stock1.refresh_from_db()
+        self.assertEqual(stock1.current_capacity, 100)
+        stock2.refresh_from_db()
+        self.assertEqual(stock2.current_capacity, 50)
+
 class RestockTestCase(APITestCase):
     def setUp(self):
         self.store = Store.objects.create(store_name='My Store',user=User.objects.create(user_id=1))
