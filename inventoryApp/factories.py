392b802f3cb419b02bbef5fbe43164ea5abb527f
fikrirohizam <fikri@infin8co.com>
2023-04-06 14:18:40 +0800

    add factory for Store with post_generation

--- inventoryApp/factories.py
+++ inventoryApp/factories.py
@@ -1,37 +1,36 @@
-import datetime
 from factory.django import DjangoModelFactory
-from . import models
-from .models import Material, User, MaterialQuantity, MaterialStock, Store, Product
+from inventoryApp import models
 from factory import Faker
-from factory import SubFactory, RelatedFactory,Iterator, Sequence, fuzzy, LazyAttribute
+from factory import SubFactory, RelatedFactory, fuzzy, LazyAttribute
+from factory import post_generation
 
 class MaterialFactory(DjangoModelFactory):
     class Meta:
-        model = Material
+        model = models.Material
     name = Faker('first_name')
     price = fuzzy.FuzzyDecimal(0.5,100.0)
 
 class MaterialQuantityFactory(DjangoModelFactory):
     class Meta:
-        model = MaterialQuantity
+        model = models.MaterialQuantity
     quantity = fuzzy.FuzzyInteger(1,100)
     ingredient = SubFactory(MaterialFactory)
 
 class ProductFactory(DjangoModelFactory):
     class Meta:
-        model = Product
+        model = models.Product
     material_quantity = RelatedFactory(MaterialQuantityFactory)
     name = Faker('name')
 
 class UserFactory(DjangoModelFactory):
     class Meta:
-        model = User
+        model = models.User
     user_id = fuzzy.FuzzyInteger(1,100)
     username = Faker('name')
 
 class MaterialStockFactory(DjangoModelFactory):
     class Meta:
-        model = MaterialStock
+        model = models.MaterialStock
     store = SubFactory('inventoryApp.factories.StoreFactory')
     material = SubFactory(MaterialFactory)
     max_capacity = fuzzy.FuzzyInteger(1,100)
@@ -39,9 +38,31 @@ class MaterialStockFactory(DjangoModelFactory):
 
 class StoreFactory(DjangoModelFactory):
     class Meta:
-        model = Store
+        model = models.Store
     store_name = Faker('company')
     products = RelatedFactory(ProductFactory)
     user = SubFactory(UserFactory)
+
+
+# Does not create new product if it is called with added products
+# e.g. StoreFactory(products=[self.product])
+class StoreWithProductsFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Store
+    store_name = Faker('company')
+    user = SubFactory(UserFactory)
+
+    @post_generation
+    def products(self, create, extracted, **kwargs):
+        if not create:
+            return
+        
+        if extracted:
+            # Use the provided products instead of creating new ones.
+            self.products.set(extracted)
+            return
+
+        # Create a default product for the store.
+        self.products.add(ProductFactory())
 
 
