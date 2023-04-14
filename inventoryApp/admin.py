8593bf3b96b798dca7cf0f168cf99f458957e67e
fikrirohizam <fikri@infin8co.com>
2023-04-14 12:29:16 +0800

    add SalesHistory and SalesHistoryProduct to Admin

--- inventoryApp/admin.py
+++ inventoryApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Store, Product, Material, MaterialQuantity, MaterialStock
+from .models import User, Store, Product, Material, MaterialQuantity, MaterialStock, SalesHistory, SalesHistoryProduct
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
@@ -9,3 +9,6 @@ admin.site.register(Product)
 admin.site.register(Material)
 admin.site.register(MaterialQuantity)
 admin.site.register(MaterialStock)
+admin.site.register(SalesHistory)
+admin.site.register(SalesHistoryProduct)
+
